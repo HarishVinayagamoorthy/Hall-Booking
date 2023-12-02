358f38ee8da60e6e0dc1ed387d12ea04dc167374
HarishVinayagamoorthy <harishvinayagamoorthy123@gmail.com>
2023-12-02 12:01:37 +0000

    ui update

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-
+const router = express.Router();
 app.use(express.json());
 app.use("/rooms", roomRoutes);
+app.use("/", roomRoutes);
 
-
+  
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -4,8 +4,10 @@ import  roomController from '../Controllers/roomController.js';
 
 const router = express.Router();
 
-
-router.get('/',roomController.getAllRooms)
+router.get("/", (req, res) => {
+  res.status(200).send(` <h1>Welcome to Backend of Hall Booking project</h1>`);
+});
+router.get("/getAllRooms", roomController.getAllRooms);
 router.post('/createRoom', roomController.createRoom);
 router.post('/bookRoom', roomController.bookRoom);
 router.get('/listRooms', roomController.listRooms);
